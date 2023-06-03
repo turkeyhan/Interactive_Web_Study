28c9df79bbdf9d36a536a97d0804ef3100238bff
turkeyhan <hski0930@gmail.com>
2023-06-03 20:16:25 +0900

    Week_9
    Week_9

---
+++ Week_9/12-template/js/main.js
@@ -0,0 +1,81 @@
+const frame = document.querySelector("section");
+const lists = frame.querySelectorAll("article");
+const audio = frame.querySelectorAll("audio");
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+const deg = 45; 
+const len = lists.length-1; 
+let i = 0; 
+let num = 0; 
+let active = 0; 
+
+function activation(index, lists){
+    for( let el of lists){
+        el.classList.remove("on");
+    }
+    lists[index].classList.add("on");
+}
+
+for(let el of lists) {  
+    let pic = el.querySelector(".pic");  
+    el.style.transform = `rotate(${deg* i}deg) translateY(-100vh)`;
+    pic.style.backgroundImage = `url(img/member${i+1}.jpg)`;
+    i++; 
+
+    let play = el.querySelector(".play");
+    let pause = el.querySelector(".pause");
+    let load = el.querySelector(".load");
+
+    play.addEventListener("click", e=>{
+        let isActive = e.currentTarget.closest("article").classList.contains("on");
+        if(isActive){
+            e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+            e.currentTarget.closest("article").querySelector("audio").play(); 
+        }              
+    });
+
+    pause.addEventListener("click", e=>{
+        let isActive = e.currentTarget.closest("article").classList.contains("on");
+        if(isActive){
+            e.currentTarget.closest("article").querySelector(".pic").classList.remove("on");
+            e.currentTarget.closest("article").querySelector("audio").pause();
+        } 
+    });
+
+    load.addEventListener("click", e=>{
+        let isActive = e.currentTarget.closest("article").classList.contains("on");
+        if(isActive){
+            e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+            e.currentTarget.closest("article").querySelector("audio").load();   
+            e.currentTarget.closest("article").querySelector("audio").play(); 
+        }          
+    });
+}
+
+function initMusic(){
+    for( let el of audio ){
+        el.pause();
+        el.load();
+        el.closest("article").querySelector(".pic").classList.remove("on");
+    }
+}
+
+prev.addEventListener("click", ()=>{
+    initMusic();
+
+    num++;  
+    frame.style.transform = `rotate(${deg* num}deg)`;    
+
+    (active == 0 ) ? active = len : active--;
+    activation(active, lists);    
+});
+
+next.addEventListener("click", ()=>{
+    initMusic();
+
+    num--;   
+    frame.style.transform = `rotate(${deg* num}deg)`;   
+
+    (active == len ) ? active = 0 : active++; 
+    activation(active, lists);
+});
---
+++ Week_9/Note.md
@@ -0,0 +1,7 @@
+# 12. 뮤직 플레이어 제작하기  
+## Tips  
+* javascript를 통한 제어  
+* hover는 기본적으로 css에서 있어보이는 꾸미기, transition 또한 그럼  
+* queryselector로 html에서 불러오고 어떤 이벤트에 대한 요소에 바꾼 기능 덧씌우기  
+* 음악을 멈추고 새로고침하고 재생하고에 따른 동작을 js에서 제어  
+* keyframes를 통한 animation 효과  
